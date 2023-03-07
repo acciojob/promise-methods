f9e89c5bebf1b8d64deea73cfd82ef5e4897c883
HarshKhandelwal101 <78348500+HarshKhandelwal101@users.noreply.github.com>
2023-03-07 18:27:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-//your JS code here. If required.
+function myRace(promises) {
+  // implementation
+}
+
+function myAny(promises) {
+  // implementation
+}
+
+function myAll(promises) {
+  // implementation
+}
+
+function myAllSettled(promises) {
+  // implementation
+}
+
+module.exports = {
+  myRace,
+  myAny,
+  myAll,
+  myAllSettled
+};
